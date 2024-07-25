dd30a2214ac4db8ca52fe0ae4906d4964b7381da
InKyungWoo <chelsea1201@naver.com>
2024-07-25 10:57:04 +0900

    :sparkles: feat: Engance map controls - current location, zoomin/out

--- src/components/AppleMap.jsx
+++ src/components/AppleMap.jsx
@@ -1,9 +1,13 @@
-import React, { useState, useEffect } from 'react';
-import { View, StyleSheet, Dimensions } from 'react-native';
+import React, { useState, useEffect, useRef } from 'react';
+import { View, StyleSheet, Dimensions, Image, TouchableOpacity } from 'react-native';
 import MapView from 'react-native-maps';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import Geolocation from 'react-native-geolocation-service';
 
 const { width, height } = Dimensions.get('window');
+const currentLocation = require('../assets/icons/currentLocation.png');
+const zoominIcon = require('../assets/icons/zoomIn.png');
+const zoomOutIcon = require('../assets/icons/zoomOut.png');
 
 const AppleMap = () => {
     const [region, setRegion] = useState({
@@ -12,36 +16,88 @@ const AppleMap = () => {
         latitudeDelta: 0.0922,
         longitudeDelta: 0.0421,
     });
+    const mapRef = useRef(null);
 
     useEffect(() => {
-        const loadLocation = async () => {
-            try {
-                const savedLocation = await AsyncStorage.getItem('userLocation');
-                if (savedLocation) {
-                    const parsedLocation = JSON.parse(savedLocation);
-                    setRegion({
-                        ...region,
-                        latitude: parsedLocation.latitude,
-                        longitude: parsedLocation.longitude,
-                    });
-                }
-            } catch (e) {
-                console.error('저장된 위치 load 실패', e);
-            }
-        };
-
         loadLocation();
     }, []);
+
+    const loadLocation = async () => {
+        try {
+            const savedLocation = await AsyncStorage.getItem('userLocation');
+            if (savedLocation) {
+                const parsedLocation = JSON.parse(savedLocation);
+                setRegion({
+                    ...region,
+                    latitude: parsedLocation.latitude,
+                    longitude: parsedLocation.longitude,
+                });
+            }
+        } catch (e) {
+            console.error('저장된 위치 load 실패', e);
+        }
+    };
+
+    // 현재 위치로 이동
+    const goToCurrentLocation = () => {
+        Geolocation.getCurrentPosition(position => {
+            const { latitude, longitude } = position.coords;
+            mapRef.current.animateToRegion(
+                {
+                    latitude,
+                    longitude,
+                },
+                1000,
+            );
+        });
+    };
+
+    // 줌인 & 줌아웃
+    const zoomIn = () => {
+        const newRegion = {
+            ...region,
+            latitudeDelta: region.latitudeDelta / 2,
+            longitudeDelta: region.longitudeDelta / 2,
+        };
+        mapRef.current.animateToRegion(newRegion, 300);
+    };
+
+    const zoomOut = () => {
+        const newRegion = {
+            ...region,
+            latitudeDelta: region.latitudeDelta * 2,
+            longitudeDelta: region.longitudeDelta * 2,
+        };
+        mapRef.current.animateToRegion(newRegion, 300);
+    };
 
     return (
         <View style={styles.container}>
             <MapView
+                ref={mapRef}
                 style={styles.map}
                 region={region}
                 onRegionChangeComplete={setRegion}
                 showsUserLocation={true}
-                showsMyLocationButton={true}
+                showsMyLocationButton={false}
+                zoomEnabled={true}
+                zoomControlEnabled={true}
             />
+            <View style={styles.buttonContainer}>
+                <View style={{ gap: 5 }}>
+                    <TouchableOpacity onPress={zoomIn}>
+                        <Image source={zoominIcon} style={styles.buttonStyle} />
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={zoomOut}>
+                        <Image source={zoomOutIcon} style={styles.buttonStyle} />
+                    </TouchableOpacity>
+                </View>
+                <View>
+                    <TouchableOpacity onPress={goToCurrentLocation}>
+                        <Image source={currentLocation} style={styles.buttonStyle} />
+                    </TouchableOpacity>
+                </View>
+            </View>
         </View>
     );
 };
@@ -54,6 +110,15 @@ const styles = StyleSheet.create({
     },
     map: {
         ...StyleSheet.absoluteFillObject,
+    },
+    buttonContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        padding: 10,
+    },
+    buttonStyle: {
+        width: 30,
+        height: 30,
     },
 });
 
